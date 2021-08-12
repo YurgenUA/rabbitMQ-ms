12639324ab7a68817850a332324c40cd36fadfda
YurgenUA <yuri@print.com>
2021-08-12 20:17:12 +0300

    implement BankingTransferCommand, including sending message to RabbitMQ

--- MicroRabbit.Infra.IoC/DependencyContainer.cs
+++ MicroRabbit.Infra.IoC/DependencyContainer.cs
@@ -1,4 +1,12 @@
-﻿using MicroRabbit.Domain.Core.Bus;
+﻿using MediatR;
+using MicroRabbit.Banking.Application.Interfaces;
+using MicroRabbit.Banking.Application.Services;
+using MicroRabbit.Banking.Data.Context;
+using MicroRabbit.Banking.Data.Repository;
+using MicroRabbit.Banking.Domain.CommandHandlers;
+using MicroRabbit.Banking.Domain.Commands;
+using MicroRabbit.Banking.Domain.Interfaces;
+using MicroRabbit.Domain.Core.Bus;
 using MicroRabbit.Infra.Bus;
 using Microsoft.Extensions.DependencyInjection;
 using System;
@@ -12,6 +20,15 @@ namespace MicroRabbit.Infra.IoC
             //Domain bus
             services.AddTransient<IEventBus, RabbitMQBus>();
 
+            //Application Services
+            services.AddTransient<IAccountService, AccountService>();
+
+            //Data
+            services.AddTransient<IAccountRepository, AccountRepository>();
+            services.AddTransient<BankingDBContext>();
+
+            //Domain banking commands
+            services.AddTransient<IRequestHandler<CreateTransferCommand, bool>, TransferCommandHandler>();
         }
     }
 }
